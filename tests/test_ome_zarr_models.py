df974a682bda514872fd4ea76d6c36a579cad27e
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2026-04-15 17:47:57 +0200

    remove duplicate code

--- tests/test_ome_zarr_models.py
+++ tests/test_ome_zarr_models.py
@@ -1,11 +1,22 @@
-from ome_zarr_models._v06.coordinate_transforms import Identity, Translation
+from ome_zarr_models._v06.coordinate_transforms import Identity
 from ngff_transform_protocols.protocols.transform_protocols import IdentityProtocol, TransformProtocol
 
 from typing import Literal, NamedTuple
 
+from typing import Literal, NamedTuple
+
+class FakeTransform(NamedTuple):
+    invalid_name: str
+    type: Literal["identity"]
+    input: str
+    output: str
+
 def test_identity_protocol():
     transform = Identity(name="example", type="identity", input="input_cs", output="output_cs")
-    assert isinstance(transform, TransformProtocol)
+    assert isinstance(transform, IdentityProtocol)
+
+    transform = FakeTransform(invalid_name="example", type="identity", input="input_cs", output="output_cs")
+    assert not isinstance(transform, IdentityProtocol)
 
 
     # transform = FakeTransform(name="example", type="identity", input="input_cs", output="output_cs")
