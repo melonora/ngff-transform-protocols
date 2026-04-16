f393e51a0dcdc52b09a8ca08cd4212b517c3d7ba
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2026-04-16 10:08:27 +0200

    minor cleanup

--- tests/test_ome_zarr_models.py
+++ tests/test_ome_zarr_models.py
@@ -3,8 +3,6 @@ from ngff_transform_protocols.protocols.transform_protocols import IdentityProto
 
 from typing import Literal, NamedTuple
 
-from typing import Literal, NamedTuple
-
 class FakeTransform(NamedTuple):
     invalid_name: str
     type: Literal["identity"]
@@ -14,15 +12,3 @@ class FakeTransform(NamedTuple):
 def test_identity_protocol():
     transform = Identity(name="example", type="identity", input="input_cs", output="output_cs")
     assert isinstance(transform, IdentityProtocol)
-
-    transform = FakeTransform(invalid_name="example", type="identity", input="input_cs", output="output_cs")
-    assert not isinstance(transform, IdentityProtocol)
-
-
-    # transform = FakeTransform(name="example", type="identity", input="input_cs", output="output_cs")
-    # protocol_members = set(IdentityProtocol.__protocol_attrs__)
-    # missing = [m for m in protocol_members if not hasattr(transform, m)]
-    # print("Missing members:", missing)
-    #
-    # assert isinstance(transform, IdentityProtocol)
-
